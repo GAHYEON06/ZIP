dfae877cfa51cc665458486cd88b89782205b0e1
GAHYEON06 <smily.price1004@gmail.com>
2026-07-28 17:37:41 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from '@tanstack/react-router'
 import { getRouter } from './router'
+import './styles.css' // CSS 글로벌 스타일 불러오기
 
 // router 인스턴스 생성
 const router = getRouter()
